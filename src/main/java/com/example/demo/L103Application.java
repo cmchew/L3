13db598648183f806ed8d0a34144d4212b7815eb
cmchew <write_on@hotmail.com>
2020-01-06 16:29:32 -0500

    L103

---
+++ src/main/java/com/example/demo/L103Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L103Application.class, args);
+    }
+
+}
